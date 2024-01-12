134a39ea6907644908543124aec2d39740b17ea0
RXGabriel <gabriel2005rf@gmail.com>
2024-01-12 11:03:14 -0300

    separando as views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-  res.send("home page");
+  res.render("pages/page");
 };
-export const dogs = (req: Request, res: Response) => {};
-export const cats = (req: Request, res: Response) => {};
-export const fishes = (req: Request, res: Response) => {};
+export const dogs = (req: Request, res: Response) => {
+  res.send("racas de cachorros");
+};
+export const cats = (req: Request, res: Response) => {
+  res.send("racas de gatos");
+};
+export const fishes = (req: Request, res: Response) => {
+  res.send("racas de peixes");
+};
