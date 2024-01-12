0cf01ec168452f7946b1bd079cb45f7582a15622
RXGabriel <gabriel2005rf@gmail.com>
2024-01-12 11:16:47 -0300

    Manipulando dados em  cada página

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,35 @@
 import { Request, Response } from "express";
+import { title } from "process";
 
 export const home = (req: Request, res: Response) => {
-  res.render("pages/page");
+  res.render("pages/page", {
+    banner: {
+      title: "Todos os animais",
+      background: "allanimals.jpg",
+    },
+  });
 };
 export const dogs = (req: Request, res: Response) => {
-  res.send("racas de cachorros");
+  res.render("pages/page", {
+    banner: {
+      title: "Cachorros",
+      background: "banner_dog.webp",
+    },
+  });
 };
 export const cats = (req: Request, res: Response) => {
-  res.send("racas de gatos");
+  res.render("pages/page", {
+    banner: {
+      title: "Gatos",
+      background: "banner_cat.jpg",
+    },
+  });
 };
 export const fishes = (req: Request, res: Response) => {
-  res.send("racas de peixes");
+  res.render("pages/page", {
+    banner: {
+      title: "Peixes",
+      background: "banner_fish.jpg",
+    },
+  });
 };
